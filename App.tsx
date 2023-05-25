326ede5fe846f117bb852e7c5b97989a7f76f0b5
ali <ali412514n@gmail.com>
2023-05-25 13:57:25 +0430

    + imgs

--- App.tsx
+++ App.tsx
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, Image, StyleSheet, Text, View, Button, Alert, Platform, StatusBar } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+      </View>
+      <Button title='Tap me' onPress={()=>Alert.alert("Good Morning!", "How do you feel today?", [
+        { text: "Cancel",  },
+        { text: "Prefom",  },
+      ])} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: "white",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightgreen',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  img: {
+    flex: 1,
+    height: undefined,
+    width: "100%",
+    aspectRatio: 1,
+  }
 });
