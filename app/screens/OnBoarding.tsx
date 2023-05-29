f558cd36a63a2ccb3e6f040e7a6a73d5cec22481
ali <ali412514n@gmail.com>
2023-05-29 19:47:05 +0430

    fixed flexes

--- app/screens/OnBoarding.tsx
+++ app/screens/OnBoarding.tsx
@@ -1,9 +1,19 @@
-import { Box, Text, Flex, FlatList, Image, Button } from "native-base";
+import {
+  Box,
+  Button,
+  FlatList,
+  Flex,
+  HStack,
+  Image,
+  Stack,
+  Text,
+  VStack,
+} from "native-base";
 import React, { useRef, useState } from "react";
 import {
+  Animated,
   ImageSourcePropType,
   useWindowDimensions,
-  Animated,
   View,
 } from "react-native";
 
@@ -21,7 +31,7 @@ export default function OnBoarding() {
   const viewableItemsChanged = useRef(
     ({ viewableItems }: { viewableItems: any[] }) => {
       setCurrentIndex(viewableItems[0].index);
-    }
+    },
   ).current;
 
   const slides: Slide[] = [
@@ -36,8 +46,8 @@ export default function OnBoarding() {
       img: require("../assets/img/boarding/i2.png"),
     },
     {
-      title: "thrid page",
-      description: "the thrid page desc",
+      title: "third page",
+      description: "the third page desc",
       img: require("../assets/img/boarding/i3.png"),
     },
     {
@@ -49,30 +59,35 @@ export default function OnBoarding() {
 
   return (
     <Box flex={1}>
-      <FlatList
-        data={slides}
-        keyExtractor={(item) => item.title}
-        renderItem={({ item }) => (
-          <OnBoardingSlide
-            title={item.title}
-            description={item.description}
-            img={item.img}
-          />
-        )}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        bounces={false}
-        onScroll={Animated.event(
-          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
-          {
-            useNativeDriver: false,
-          }
-        )}
-        scrollEventThrottle={32}
-        onViewableItemsChanged={viewableItemsChanged}
-        ref={slideRef}
-      />
-      <OnBoardingFooter data={slides} scrollX={scrollX} />
+      <Box flex={4}>
+        <FlatList
+          flex={3}
+          data={slides}
+          keyExtractor={(item) => item.title}
+          renderItem={({ item }) => (
+            <OnBoardingSlide
+              title={item.title}
+              description={item.description}
+              img={item.img}
+            />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          bounces={false}
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+            {
+              useNativeDriver: false,
+            },
+          )}
+          scrollEventThrottle={32}
+          onViewableItemsChanged={viewableItemsChanged}
+          ref={slideRef}
+        />
+      </Box>
+      <Box flex={1}>
+        <OnBoardingFooter data={slides} scrollX={scrollX} />
+      </Box>
     </Box>
   );
 }
@@ -89,9 +104,9 @@ export function OnBoardingSlide({
   const { height, width } = useWindowDimensions();
 
   return (
-    <Flex w={width} bg="white">
-      <Image source={img} w="full" alt="img" flex={0.8} mb="3" />
-      <Box flex={0.2} alignItems={"center"}>
+    <Flex w={width} alignItems={"center"}>
+      <Image source={img} h="full" resizeMode="contain" alt="img" flex={7} />
+      <Box alignItems={"center"} flex={1}>
         <Text fontSize={"lg"} color={"primary.700"} fontWeight={"bold"}>
           {title}
         </Text>
@@ -111,8 +126,8 @@ export function OnBoardingFooter({
   const { width } = useWindowDimensions();
 
   return (
-    <Flex alignItems={"center"} flex={0.4} bg="white">
-      <Flex flexDir={"row"} my="10">
+    <Stack alignItems={"center"} space="xl">
+      <Flex flexDir={"row"}>
         {data.map((_, i) => {
           const inputRange = [(i - 1) * width, i * width, (i + 1) * width];
           const dotWidth = scrollX.interpolate({
@@ -126,7 +141,7 @@ export function OnBoardingFooter({
               style={{
                 height: 10,
                 borderRadius: 5,
-                backgroundColor: "black",
+                backgroundColor: "#333",
                 marginHorizontal: 4,
                 width: dotWidth,
               }}
@@ -137,6 +152,6 @@ export function OnBoardingFooter({
       <Button w="20" h="20" borderRadius={"full"}>
         Touchme
       </Button>
-    </Flex>
+    </Stack>
   );
 }
