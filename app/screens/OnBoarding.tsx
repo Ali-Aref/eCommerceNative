bb53f9ce13f50861ff80d8df57a4a2ecda5da3d7
ali <ali412514n@gmail.com>
2023-05-29 16:17:46 +0430

    + indicator and btn

--- app/screens/OnBoarding.tsx
+++ app/screens/OnBoarding.tsx
@@ -1,78 +1,142 @@
-import { Box, Button, Text, Flex , FlatList, Image, Stack } from 'native-base';
-import React, { useRef, RefObject } from 'react'
-import { ImageSourcePropType, useWindowDimensions } from 'react-native';
-import ChooseLanguage from './ChooseLanguage';
+import { Box, Text, Flex, FlatList, Image, Button } from "native-base";
+import React, { useRef, useState } from "react";
+import {
+  ImageSourcePropType,
+  useWindowDimensions,
+  Animated,
+  View,
+} from "react-native";
 
 interface Slide {
-    title: string;
-    description: string;
-    img: ImageSourcePropType;
+  title: string;
+  description: string;
+  img: ImageSourcePropType;
 }
 
 export default function OnBoarding() {
-  const slides: Slide[]  = [                
+  const slideRef = useRef(null);
+  const scrollX = useRef(new Animated.Value(0)).current;
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const viewableItemsChanged = useRef(
+    ({ viewableItems }: { viewableItems: any[] }) => {
+      setCurrentIndex(viewableItems[0].index);
+    }
+  ).current;
+
+  const slides: Slide[] = [
     {
-        title: "First Page",
-        description: "The first page desc",
-        img: require("../assets/img/boarding/i1.png"),
+      title: "First Page",
+      description: "The first page desc",
+      img: require("../assets/img/boarding/i1.png"),
     },
     {
-        title: "second page",
-        description: "the second page desc",
-        img: require("../assets/img/boarding/i2.png"),
-    }, 
+      title: "second page",
+      description: "the second page desc",
+      img: require("../assets/img/boarding/i2.png"),
+    },
     {
-        title: "thrid page",
-        description: "the thrid page desc",
-        img: require("../assets/img/boarding/i3.png"),
-    }, 
+      title: "thrid page",
+      description: "the thrid page desc",
+      img: require("../assets/img/boarding/i3.png"),
+    },
     {
-        title: "fourth page",
-        description: "the fourth page desc",
-        img: require("../assets/img/boarding/i4.png"),
-    }, 
-    ]
+      title: "fourth page",
+      description: "the fourth page desc",
+      img: require("../assets/img/boarding/i4.png"),
+    },
+  ];
 
   return (
     <Box flex={1}>
-        <FlatList 
-            data={slides} 
-            keyExtractor={(item)=> item.title}
-            renderItem={(({ item })=>(
-                <OnBoardingSlide  title={item.title} description={item.description} img={item.img} />
-            ))} 
-            horizontal
-            showsHorizontalScrollIndicator
-            bounces={false}
-        />
-        <OnBoardingFooter />
+      <FlatList
+        data={slides}
+        keyExtractor={(item) => item.title}
+        renderItem={({ item }) => (
+          <OnBoardingSlide
+            title={item.title}
+            description={item.description}
+            img={item.img}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        bounces={false}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+          {
+            useNativeDriver: false,
+          }
+        )}
+        scrollEventThrottle={32}
+        onViewableItemsChanged={viewableItemsChanged}
+        ref={slideRef}
+      />
+      <OnBoardingFooter data={slides} scrollX={scrollX} />
     </Box>
-  )
+  );
 }
 
-export function OnBoardingSlide({ backgroundColor="white", img, title, description }:{
-    backgroundColor?: string;
-    img: ImageSourcePropType;
-    title: string;
-    description: string;
+export function OnBoardingSlide({
+  img,
+  title,
+  description,
+}: {
+  img: ImageSourcePropType;
+  title: string;
+  description: string;
 }) {
-    const { height, width } = useWindowDimensions()
+  const { height, width } = useWindowDimensions();
 
-    return <Flex alignItems={"center"}  bg={backgroundColor} w={width} >
-        <Image source={img} w="full" resizeMode="contain"  flex={0.6} alt="img" />
-        <Text fontSize={"lg"} color={'primary.700'} fontWeight={"bold"}>{title}</Text>
+  return (
+    <Flex w={width} bg="white">
+      <Image source={img} w="full" alt="img" flex={0.8} mb="3" />
+      <Box flex={0.2} alignItems={"center"}>
+        <Text fontSize={"lg"} color={"primary.700"} fontWeight={"bold"}>
+          {title}
+        </Text>
         <Text fontSize={"md"}>{description}</Text>
+      </Box>
     </Flex>
-
+  );
 }
 
-export function OnBoardingFooter({}){
-    const boardBtn = (label: string) => {
-        return <Button>{label}</Button>
-    }
-    return <Flex flexDir={"row"} justifyContent={"center"} my="4">
-        {boardBtn("one")}
-        {boardBtn("two")}
-    </Flex>
+export function OnBoardingFooter({
+  data,
+  scrollX,
+}: {
+  data: Slide[];
+  scrollX: Animated.Value;
+}) {
+  const { width } = useWindowDimensions();
 
+  return (
+    <Flex alignItems={"center"} flex={0.4} bg="white">
+      <Flex flexDir={"row"} my="10">
+        {data.map((_, i) => {
+          const inputRange = [(i - 1) * width, i * width, (i + 1) * width];
+          const dotWidth = scrollX.interpolate({
+            inputRange,
+            outputRange: [10, 20, 10],
+            extrapolate: "clamp",
+          });
+          return (
+            <Animated.View
+              key={i.toString()}
+              style={{
+                height: 10,
+                borderRadius: 5,
+                backgroundColor: "black",
+                marginHorizontal: 4,
+                width: dotWidth,
+              }}
+            />
+          );
+        })}
+      </Flex>
+      <Button w="20" h="20" borderRadius={"full"}>
+        Touchme
+      </Button>
+    </Flex>
+  );
 }
