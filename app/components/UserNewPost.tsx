e8022817ae00675503e4b8383a0e85e71eef7734
Ali <ali412514n@gmial.com>
2023-06-21 19:05:03 +0430

    added UserNewPostScreen

---
+++ app/components/UserNewPost.tsx
@@ -0,0 +1,10 @@
+import { Box, Text } from "native-base";
+import React from "react";
+
+export const UserNewPost = (props: {}) => {
+  return (
+    <Box>
+      <Text>NEW POST</Text>
+    </Box>
+  );
+};
